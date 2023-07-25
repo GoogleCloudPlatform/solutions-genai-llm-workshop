be7e18f352d83fb6e3e0915050abe6037f7620d2
Michael Chi <kalschi@google.com>
2023-07-25 00:15:32 +0000

    Update LAB, use Chinese Wiki article

--- LAB002-1-WebContent/1-UnstructuredURLLoader.py
+++ LAB002-1-WebContent/1-UnstructuredURLLoader.py
@@ -17,12 +17,12 @@ from langchain.llms.vertexai import VertexAI
 
 llm = VertexAI(temperature=0.2, max_output_tokens=1024)
 
-urls = ["https://tw.news.yahoo.com/%E5%8F%AA%E6%8E%A5%E5%BE%85%E5%8F%B0%E7%81%A3%E9%81%8A%E5%AE%A2-77%E6%AD%B2%E6%97%A5%E6%9C%AC%E7%88%BA%E7%88%BA-%E5%85%8D%E8%B2%BB-%E7%95%B6%E5%B0%8E%E9%81%8A-%E6%97%A5%E6%9A%A2%E9%81%8A%E4%BC%8A%E8%B1%86%E9%AB%98%E5%8E%9F-091514801.html"]
+urls = ["https://zh.wikipedia.org/wiki/Bard_(%E8%81%8A%E5%A4%A9%E6%A9%9F%E5%99%A8%E4%BA%BA)"]
 
 loader = UnstructuredURLLoader(urls=urls)
 documents = loader.load_and_split()
 
-query = "簡單總結一下這個新聞"
+query = "請用300字簡單描述Bard的發展"
 chain = load_qa_chain(llm=llm, chain_type="stuff", verbose=True)
 answer = chain.run(input_documents=documents, question=query)
 print(answer)
